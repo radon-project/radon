108ecaaa11696f5caeea0c0f6c067d25a9d27e24
Md. Almas Ali <almaspr3@gmail.com>
2021-11-17 19:40:02 +0600

    Fixed theme detection issue.

--- backend/templatetags/theme_details.py
+++ backend/templatetags/theme_details.py
@@ -7,10 +7,13 @@ register = template.Library()
 
 
 def theme_details(name):
-    theme = glob.glob1(dirname=f'rn-themes/{name}/', pattern='theme.json')[0]
-    with open(f'rn-themes/{name}/theme.json', 'r') as js:
-        js = js.read()
-    theme = json.loads(js)
+    theme = glob.glob1(dirname=f'rn-themes/{name}/', pattern='theme.json')
+    if theme is not []:
+        with open(f'rn-themes/{name}/theme.json', 'r') as js:
+            js = js.read()
+        theme = json.loads(js)
+    else:
+        theme = None
     return theme
 
 # os.chdir('../')
@@ -21,4 +24,17 @@ def theme_details(name):
 
 @register.filter(name='theme_name')
 def theme_name(name):
-    return theme_details(name)['RN_THEME']['NAME']
+    try:
+        theme = theme_details(name)['RN_THEME']['NAME']
+    except:
+        theme = name
+    return theme
+
+
+@register.filter(name='theme_description')
+def theme_description(name):
+    try:
+        theme = theme_details(name)['RN_THEME']['DESCRIPTION']
+    except:
+        theme = 'Broken Theme'
+    return theme
