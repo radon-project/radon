a770afbba983d9e7756c1da5fb0e373f78a1ccc6
Md. Almas Ali <almaspr3@gmail.com>
2023-07-11 02:17:29 +0600

    Added inline code execution, add version check support, bugfix.

--- radon.py
+++ radon.py
@@ -9,7 +9,7 @@ import core as base_core
 def shell():
     print('Radon 0.0.1\nPress Ctrl+C to exit()')
     brace_count = 0
-    
+
     while True:
         try:
             text = input('>>> ')
@@ -22,14 +22,13 @@ def shell():
                     text += '\n' + input('... ')
                     if text.strip()[-1] == '{':
                         brace_count += 1
-                    elif text.strip()[-1] == '}':
+                    elif text.strip()[-1] == '}' or \
+                            text.strip()[0] == '}':
                         brace_count -= 1
 
                     if brace_count == 0:
                         break
 
-                    
-            
             result, error, should_exit = base_core.run('<stdin>', text)
 
             if error:
@@ -45,12 +44,18 @@ def shell():
         except KeyboardInterrupt:
             print('KeyboardInterrupt')
 
+
 parser = argparse.ArgumentParser(description='Radon programming language')
 parser.add_argument('-s', '--source', type=str, help='Radon source file')
+parser.add_argument('-c', '--command', type=str,
+                    help='Command to execute as string')
+parser.add_argument('-v', '--version', help='Version info',
+                    action='store_true')
 args = parser.parse_args()
 
 if args.source:
-    result, error, should_exit = base_core.run('<stdin>', f'require("{args.source}")')
+    result, error, should_exit = base_core.run(
+        '<stdin>', f'require("{args.source}")')
 
     if error:
         print(error.as_string())
@@ -62,6 +67,20 @@ if args.source:
 
     if should_exit:
         exit()
+
+elif args.command:
+    result, error, should_exit = base_core.run('<stdin>', args.command)
+
+    if error:
+        print(error.as_string())
+    elif result:
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
+
+elif args.version:
+    print(base_core.__version__)
 
 else:
     shell()
