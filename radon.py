71468a7e93806b1dc16b82521b90f26bc33bf822
angelcaru <angelcaru10@gmail.com>
2024-04-05 16:30:14 +0200

    Fix trailing exit code bug (#12)

--- radon.py
+++ radon.py
@@ -71,11 +71,6 @@ if args.source:
 
     if error:
         print(error.as_string())
-    elif result:
-        if len(result.elements) == 1:
-            print(repr(result.elements[0]))
-        else:
-            print(repr(result))
 
     if should_exit:
         exit()
