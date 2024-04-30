3ae3ee17e5a1fea2aaf4c0cda3bd48d5bd7a75a5
Md. Almas Ali <almaspr3@gmail.com>
2024-04-30 18:20:39 +0600

    feat: added Requests() to global_symbol_table.

--- core/builtin_classes/__init__.py
+++ core/builtin_classes/__init__.py
@@ -3,8 +3,10 @@ from core.builtin_funcs import global_symbol_table
 from core.builtin_classes.file_object import FileObject
 from core.builtin_classes.string_object import StringObject
 from core.builtin_classes.json_object import JSONObject
+from core.builtin_classes.requests_object import RequestsObject
 
 
 global_symbol_table.set("File", BuiltInClass("File", FileObject))
 global_symbol_table.set("String", BuiltInClass("String", StringObject))
 global_symbol_table.set("Json", BuiltInClass("Json", JSONObject))
+global_symbol_table.set("Requests", BuiltInClass("Requests", RequestsObject))
