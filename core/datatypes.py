f53b9831dca97da82ea00850b412eae66a419445
Md. Almas Ali <almaspr3@gmail.com>
2024-07-21 20:29:48 +0600

    fixes: reverted Boolean to String and Array comparison; default selected.

--- core/datatypes.py
+++ core/datatypes.py
@@ -339,9 +339,9 @@ class Boolean(Value):
         elif isinstance(other, Number):
             return Boolean(self.value == other.value).set_context(self.context), None
         elif isinstance(other, String):
-            return Boolean(self.value == other.value).set_context(self.context), None
+            return Boolean(False).set_context(self.context), None
         elif isinstance(other, Array):
-            return Boolean(self.value == other.elements).set_context(self.context), None
+            return Boolean(False).set_context(self.context), None
         else:
             return None, Value.illegal_operation(self, other)
 
@@ -351,9 +351,9 @@ class Boolean(Value):
         elif isinstance(other, Number):
             return Boolean(self.value != other.value).set_context(self.context), None
         elif isinstance(other, String):
-            return Boolean(self.value != other.value).set_context(self.context), None
+            return Boolean(True).set_context(self.context), None
         elif isinstance(other, Array):
-            return Boolean(self.value != other.elements).set_context(self.context), None
+            return Boolean(True).set_context(self.context), None
         else:
             return None, Value.illegal_operation(self, other)
 
