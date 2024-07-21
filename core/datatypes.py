e0e673b5cf6668b4de81247cb61720e5e3bf3157
Md. Almas Ali <almaspr3@gmail.com>
2024-07-21 20:18:57 +0600

    fixes: removed support for Number and String comparison in Radon.

--- core/datatypes.py
+++ core/datatypes.py
@@ -229,15 +229,11 @@ class Number(Value):
     def get_comparison_eq(self, other: Value) -> ResultTuple:
         if isinstance(other, Number):
             return Boolean(self.value == other.value).set_context(self.context), None
-        elif isinstance(other, String):
-            return Boolean(self.value == other.value).set_context(self.context), None
         else:
             return None, Value.illegal_operation(self, other)
 
     def get_comparison_ne(self, other: Value) -> ResultTuple:
         if isinstance(other, Number):
-            return Boolean(self.value != other.value).set_context(self.context), None
-        elif isinstance(other, String):
             return Boolean(self.value != other.value).set_context(self.context), None
         else:
             return None, Value.illegal_operation(self, other)
