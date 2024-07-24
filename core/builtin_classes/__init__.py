803ba3041809e14daca3c4e8297dde8d5e1ab174
Md. Almas Ali <almaspr3@gmail.com>
2024-07-24 20:50:25 +0600

    feat: added builtins() built-in class to manipulate built-in methods, classes, variables etc. (Super Power)

--- core/builtin_classes/__init__.py
+++ core/builtin_classes/__init__.py
@@ -1,7 +1,8 @@
 from core.builtin_classes.base_classes import BuiltInClass
+from core.builtin_classes.builtins_object import BuiltinsObject
 from core.builtin_classes.file_object import FileObject
 from core.builtin_classes.json_object import JSONObject
 from core.builtin_classes.requests_object import RequestsObject
 from core.builtin_classes.string_object import StringObject
 
-__all__ = ["BuiltInClass", "FileObject", "StringObject", "JSONObject", "RequestsObject"]
+__all__ = ["BuiltInClass", "FileObject", "StringObject", "JSONObject", "RequestsObject", "BuiltinsObject"]
--- core/builtin_funcs.py
+++ core/builtin_funcs.py
@@ -379,6 +379,7 @@ class BuiltInFunction(BaseFunction):
 
     @args(["value"])
     def execute_int(self, exec_ctx: Context) -> RTResult[Value]:
+        """Convert to Integer value."""
         value = exec_ctx.symbol_table.get("value")
         try:
             return RTResult[Value]().success(Number(int(value.value)))  # type: ignore
@@ -728,6 +729,7 @@ def create_global_symbol_table() -> SymbolTable:
     ret.set("String", bic.BuiltInClass("String", bic.StringObject.__doc__, bic.StringObject))
     ret.set("Json", bic.BuiltInClass("Json", bic.JSONObject.__doc__, bic.JSONObject))
     ret.set("Requests", bic.BuiltInClass("Requests", bic.RequestsObject.__doc__, bic.RequestsObject))
+    ret.set("builtins", bic.BuiltInClass("builtins", bic.BuiltinsObject.__doc__, bic.BuiltinsObject))
     return ret
 
 
