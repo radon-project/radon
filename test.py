0dd836cf14edfa20a2a0dd2427c648b0b188fe5f
angelcaru <angelcaru10@gmail.com>
2025-04-09 21:12:09 +0200

    refactor: improve testing framework

--- test.py
+++ test.py
@@ -35,73 +35,68 @@ def run_test(test: str) -> Output:
         proc.stderr.decode("utf-8").replace("\r\n", "\n"),
     )
 
-
-def run_tests(directory: str = "tests") -> int:
-    mypy = subprocess.Popen(
-        ["mypy", ".", "--strict"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        env=dict(**os.environ, MYPY_FORCE_COLOR="1"),
-    )
-
-    failed_tests: list[str] = []
-    for test in os.listdir(directory):
-        json_file = f"{directory}/{test}.json"
-        if not test.endswith(".rn"):
-            continue
+def run_tests_rec(tests: str, failed_tests: list[str]):
+    if os.path.isdir(tests):
+        for test in os.listdir(tests):
+            run_tests_rec(os.path.join(tests, test), failed_tests)
+    elif os.path.isfile(tests):
+        json_file = f"{tests}.json"
+        if not tests.endswith(".rn"):
+            return
         if not os.path.isfile(json_file):
             print(f"WARNING: file {json_file!r} not found, skipping...")
             print("NOTE: to create this file, run the `record` subcommand")
-            continue
+            return
 
-        print(f"Running test {test!r}...", end="", flush=True)
-        output = run_test(f"{directory}/{test}")
+        print(f"Running test {tests!r}...", end="", flush=True)
+        output = run_test(tests)
         expected_output = Output.from_file(json_file)
         if output != expected_output:
-            print(f"\rTest {test!r} failed!" + " " * 20)
+            print(f"\rTest {tests!r} failed!" + " " * 20)
             print(f"Expected: {expected_output!r}")
             print(f"Got:      {output!r}")
-            failed_tests.append(test)
+            print(f"NOTE: run `{sys.argv[0]} diff {tests}` for more information")
+            failed_tests.append(tests)
         else:
-            print(f"\rTest {test!r} passed!" + " " * 20)
+            print(f"\rTest {tests!r} passed!" + " " * 20)
+    else:
+        assert False, "unreachable"
+
+def run_tests(tests: str = "tests") -> int:
+    failed_tests: list[str] = []
+    run_tests_rec(tests, failed_tests)
 
     print()
     print("TEST SUMMARY:")
     if len(failed_tests) == 0:
         print("All tests passed!")
+        return 0
     else:
         print(f"{len(failed_tests)} tests failed:")
         for test in failed_tests:
             print(f"    {test!r}")
-
-    print("--------------")
-    print("    mypy .    ")
-    print("--------------")
-    mypy.wait()
-    assert mypy.stdout is not None
-    assert mypy.stderr is not None
-    sys.stdout.buffer.write(mypy.stdout.read())
-    sys.stderr.buffer.write(mypy.stderr.read())
-
-    if mypy.returncode == 0 and len(failed_tests) == 0:
-        return 0
-    else:
         return 1
 
-
-def record_tests(directory: str = "tests") -> int:
-    for test in os.listdir(directory):
-        if not test.endswith(".rn"):
-            continue
-        print(f"Recording {test!r}...", end="", flush=True)
-        output = run_test(f"{directory}/{test}")
+def record_tests(tests: str = "tests") -> int:
+    if os.path.isdir(tests):
+        for test in os.listdir(tests):
+            ret = record_tests(os.path.join(tests, test))
+            if ret != 0:
+                return ret
+    elif os.path.isfile(tests):
+        if not tests.endswith(".rn"):
+            return 0
+        print(f"Recording {tests!r}...", end="", flush=True)
+        output = run_test(tests)
         if os.getcwd() in (output.stdout + output.stderr):
-            print(f"\nERROR: test {test!r} accidentally depends on current directory")
+            print(f"\nERROR: test {tests!r} accidentally depends on current directory")
             return 1
-        json_file = f"{directory}/{test}.json"
+        json_file = f"{tests}.json"
         output.dump(json_file)
-        print(f"\rRecorded {test!r}" + " " * 20)
-    return 0
+        print(f"\rRecorded {tests!r}" + " " * 20)
+        return 0
+    else:
+        assert False, "Unreachable"
 
 
 def usage(program_name: str, stream: IO[str]) -> None:
@@ -109,8 +104,8 @@ def usage(program_name: str, stream: IO[str]) -> None:
         f"""Usage: {program_name} <subcommand> [args]
 SUBCOMMANDS:
     help           - Print this help message to stdout and exit successfully
-    run            - Run tests
-    record         - Record output of tests
+    run [tests]    - Run tests in directory [tests] (default: "./tests/"). Can also be used to run only a single test
+    record [tests] - Record output of tests in directory [tests] (default: "./tests/"). Can also be used to record only a single test
     diff <test.rn> - Show diff between expected and actual output
     full           - Same as `{program_name} run` + `make lint`
 """,
@@ -131,9 +126,17 @@ def main(argv: list[str]) -> int:
             usage(program_name, sys.stdout)
             return 0
         case "run":
-            return run_tests()
+            if len(argv) > 0:
+                tests_to_run = argv.pop(0)
+            else:
+                tests_to_run = "./tests/"
+            return run_tests(tests_to_run)
         case "record":
-            return record_tests()
+            if len(argv) == 0:
+                tests_to_record = argv.pop(0)
+            else:
+                tests_to_record = "./tests/"
+            return record_tests(tests_to_record)
         case "full":
             env = dict(**os.environ, FORCE_COLOR="1")
             ruff_format = subprocess.Popen(
@@ -142,15 +145,34 @@ def main(argv: list[str]) -> int:
             ruff_check = subprocess.Popen(
                 ["ruff", "check", "."], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env
             )
+            mypy = subprocess.Popen(
+                ["mypy", ".", "--strict"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                env=dict(**os.environ, MYPY_FORCE_COLOR="1"),
+            )
 
-            test_returncode = run_tests()
+            test_returncode = run_tests("./tests/")
 
-            format_ret = ruff_format.wait()
-            check_ret = ruff_check.wait()
+            print("--------------")
+            print("    mypy .    ")
+            print("--------------")
+            mypy.wait()
+            assert mypy.stdout is not None
+            assert mypy.stderr is not None
+            sys.stdout.buffer.write(mypy.stdout.read())
+            sys.stderr.buffer.write(mypy.stderr.read())
 
+            if mypy.returncode == 0 and len(failed_tests) == 0:
+                return 0
+            else:
+                return 1
             print("---------------------")
             print("ruff format --check .")
             print("---------------------")
+
+            format_ret = ruff_format.wait()
+            check_ret = ruff_check.wait()
             assert ruff_format.stdout is not None and ruff_format.stderr is not None
             sys.stdout.buffer.write(ruff_format.stdout.read())
             sys.stderr.buffer.write(ruff_format.stderr.read())
@@ -177,12 +199,12 @@ def main(argv: list[str]) -> int:
                 return 1
             test = argv.pop(0)
             try:
-                actual_output = run_test(f"tests/{test}")
+                actual_output = run_test(f"{test}")
             except FileNotFoundError:
                 print(f"ERROR: test {test!r} not found", file=sys.stderr)
                 return 1
             try:
-                expected_output = Output.from_file(f"tests/{test}.json")
+                expected_output = Output.from_file(f"{test}.json")
             except FileNotFoundError:
                 print(f"ERROR: test {test!r} has no expected output", file=sys.stderr)
                 return 1
@@ -210,4 +232,4 @@ def main(argv: list[str]) -> int:
 
 
 if __name__ == "__main__":
-    exit(main(sys.argv))
+    exit(main(sys.argv[:]))
